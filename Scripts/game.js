7bb5274188b413d2c66679c07234141af7bfc286
bir.virk90@gmail.com <jattsaab90>
2015-02-27 17:03:41 -0500

    Basic Layout

---
+++ Scripts/game.js
@@ -0,0 +1,289 @@
+﻿// CreateJS Boilerplate for COMP397
+var Button = (function () {
+    function Button(path, x, y) {
+        this._x = x;
+        this._y = y;
+        this._image = new createjs.Bitmap(path);
+        this._image.x = this._x;
+        this._image.y = this._y;
+
+        this._image.addEventListener("mouseover", this._buttonOver);
+        this._image.addEventListener("mouseout", this._buttonOut);
+    }
+    // PUBLIC PROPERTIES
+    Button.prototype.setX = function (x) {
+        this._x = x;
+    };
+
+    Button.prototype.getX = function () {
+        return this._x;
+    };
+
+    Button.prototype.setY = function (y) {
+        this._y = y;
+    };
+
+    Button.prototype.getY = function () {
+        return this._y;
+    };
+
+    Button.prototype.getImage = function () {
+        return this._image;
+    };
+
+    // PRIVATE EVENT HANDLERS
+    Button.prototype._buttonOut = function (event) {
+        event.currentTarget.alpha = 1; // 100% Alpha
+    };
+
+    Button.prototype._buttonOver = function (event) {
+        event.currentTarget.alpha = 0.5;
+    };
+    return Button;
+})();
+
+// VARIABLES ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+var canvas;
+var stage;
+var tiles = [];
+var reelContainers = [];
+
+// GAME CONSTANTS
+var NUM_REELS = 3;
+
+// GAME VARIABLES
+var playerMoney = 1000;
+var winnings = 0;
+var jackpot = 5000;
+var turn = 0;
+var playerBet = 0;
+var winNumber = 0;
+var lossNumber = 0;
+var spinResult;
+var fruits = "";
+var winRatio = 0;
+
+/* Tally Variables */
+var grapes = 0;
+var bananas = 0;
+var oranges = 0;
+var cherries = 0;
+var bars = 0;
+var bells = 0;
+var sevens = 0;
+var blanks = 0;
+
+// GAME OBJECTS
+var game;
+var background;
+var spinButton;
+var betMaxButton;
+var betOneButton;
+var resetButton;
+var powerButton;
+
+// FUNCTIONS ++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+function init() {
+    canvas = document.getElementById("canvas");
+    stage = new createjs.Stage(canvas); // Parent Object
+    stage.enableMouseOver(20); // Turn on Mouse Over events
+
+    createjs.Ticker.setFPS(60); // Set the frame rate to 60 fps
+    createjs.Ticker.addEventListener("tick", gameLoop);
+
+    main();
+}
+
+// GAMELOOP
+function gameLoop() {
+    stage.update();
+}
+
+/* Utility function to reset all fruit tallies */
+function resetFruitTally() {
+    grapes = 0;
+    bananas = 0;
+    oranges = 0;
+    cherries = 0;
+    bars = 0;
+    bells = 0;
+    sevens = 0;
+    blanks = 0;
+}
+
+/* Utility function to reset the player stats */
+function resetAll() {
+    playerMoney = 1000;
+    winnings = 0;
+    jackpot = 5000;
+    turn = 0;
+    playerBet = 0;
+    winNumber = 0;
+    lossNumber = 0;
+    winRatio = 0;
+}
+
+/* Utility function to check if a value falls within a range of bounds */
+function checkRange(value, lowerBounds, upperBounds) {
+    if (value >= lowerBounds && value <= upperBounds) {
+        return value;
+    } else {
+        return !value;
+    }
+}
+
+/* When this function is called it determines the betLine results.
+e.g. Bar - Orange - Banana */
+function Reels() {
+    var betLine = [" ", " ", " "];
+    var outCome = [0, 0, 0];
+
+    for (var spin = 0; spin < 3; spin++) {
+        outCome[spin] = Math.floor((Math.random() * 65) + 1);
+        switch (outCome[spin]) {
+            case checkRange(outCome[spin], 1, 27):
+                betLine[spin] = "blank";
+                blanks++;
+                break;
+            case checkRange(outCome[spin], 28, 37):
+                betLine[spin] = "grapes";
+                grapes++;
+                break;
+            case checkRange(outCome[spin], 38, 46):
+                betLine[spin] = "banana";
+                bananas++;
+                break;
+            case checkRange(outCome[spin], 47, 54):
+                betLine[spin] = "orange";
+                oranges++;
+                break;
+            case checkRange(outCome[spin], 55, 59):
+                betLine[spin] = "cherry";
+                cherries++;
+                break;
+            case checkRange(outCome[spin], 60, 62):
+                betLine[spin] = "bar";
+                bars++;
+                break;
+            case checkRange(outCome[spin], 63, 64):
+                betLine[spin] = "bell";
+                bells++;
+                break;
+            case checkRange(outCome[spin], 65, 65):
+                betLine[spin] = "seven";
+                sevens++;
+                break;
+        }
+    }
+    return betLine;
+}
+
+/* This function calculates the player's winnings, if any */
+function determineWinnings() {
+    if (blanks == 0) {
+        if (grapes == 3) {
+            winnings = playerBet * 10;
+        } else if (bananas == 3) {
+            winnings = playerBet * 20;
+        } else if (oranges == 3) {
+            winnings = playerBet * 30;
+        } else if (cherries == 3) {
+            winnings = playerBet * 40;
+        } else if (bars == 3) {
+            winnings = playerBet * 50;
+        } else if (bells == 3) {
+            winnings = playerBet * 75;
+        } else if (sevens == 3) {
+            winnings = playerBet * 100;
+        } else if (grapes == 2) {
+            winnings = playerBet * 2;
+        } else if (bananas == 2) {
+            winnings = playerBet * 2;
+        } else if (oranges == 2) {
+            winnings = playerBet * 3;
+        } else if (cherries == 2) {
+            winnings = playerBet * 4;
+        } else if (bars == 2) {
+            winnings = playerBet * 5;
+        } else if (bells == 2) {
+            winnings = playerBet * 10;
+        } else if (sevens == 2) {
+            winnings = playerBet * 20;
+        } else {
+            winnings = playerBet * 1;
+        }
+
+        if (sevens == 1) {
+            winnings = playerBet * 5;
+        }
+        winNumber++;
+        //showWinMessage();
+    } else {
+        lossNumber++;
+        //showLossMessage();
+    }
+}
+
+// MAIN MEAT of my code goes here
+function spinButtonClicked(event) {
+    spinResult = Reels();
+    fruits = spinResult[0] + " - " + spinResult[1] + " - " + spinResult[2];
+
+    for (var index = 0; index < NUM_REELS; index++) {
+        reelContainers[index].removeAllChildren();
+        tiles[index] = new createjs.Bitmap("assets/images/" + spinResult[index] + ".png");
+        reelContainers[index].addChild(tiles[index]);
+    }
+}
+
+function createUI() {
+    background = new createjs.Bitmap("assets/images/background.png");
+    game.addChild(background); // Add the background to the game container
+
+    for (var index = 0; index < NUM_REELS; index++) {
+        reelContainers[index] = new createjs.Container();
+        game.addChild(reelContainers[index]);
+    }
+    reelContainers[0].x = 128;
+    reelContainers[0].y = 296;
+    reelContainers[1].x = 248;
+    reelContainers[1].y = 296;
+    reelContainers[2].x = 374;
+    reelContainers[2].y = 296;
+
+    // Spin Button
+    spinButton = new Button("assets/images/spinButton.png", 410, 545);
+    game.addChild(spinButton.getImage());
+
+    // Spin Button Event Listeners
+    spinButton.getImage().addEventListener("click", spinButtonClicked);
+
+    // Bet Max Button
+    betMaxButton = new Button("assets/images/betMaxButton.png", 325, 560);
+    game.addChild(betMaxButton.getImage());
+    betMaxButton.getImage().addEventListener("click", spinButtonClicked);
+
+    // Bet One Button
+    betOneButton = new Button("assets/images/betOneButton.png", 235, 560);
+    game.addChild(betOneButton.getImage());
+    betOneButton.getImage().addEventListener("click", spinButtonClicked);
+
+    // Reset Button
+    resetButton = new Button("assets/images/resetButton.png", 150, 560);
+    game.addChild(resetButton.getImage());
+    resetButton.getImage().addEventListener("click", spinButtonClicked);
+
+    // Power Button
+    powerButton = new Button("assets/images/powerButton.png", 55, 560);
+    game.addChild(powerButton.getImage());
+    powerButton.getImage().addEventListener("click", spinButtonClicked);
+}
+
+function main() {
+    game = new createjs.Container(); // Instantiates the Game Container
+
+    createUI();
+
+    stage.addChild(game); // Adds the Game Container to the Stage
+}
+//# sourceMappingURL=game.js.map
